66ec36bc8c43f0ce6948ff017fd5307f9a826f37
Jonas Muñiz <jonasmz@gmail.com>
2024-08-08 14:00:30 -0300

    Se arreglan las importanciones

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-import { restServer } from './frameworks/restServer/interfaces/restServer.interface';
-import { AwilixDIContainer } from "./frameworks/diContainer/awilix.dicontainer";
-import { bootStrapDepencies } from "./frameworks/diContainer/dependencies/bootStrap.dependencies";
-import { ExpressRestServer } from './frameworks/restServer/express.restServer';
-import { serverConfig } from './frameworks/restServer/interfaces/serverConfig.type';
+import { AwilixDIContainer } from "./adapters/diContainer/awilix.dicontainer";
+import { bootStrapDepencies } from "./adapters/diContainer/dependencies/bootStrap.dependencies";
+import { ExpressRestServer } from "./adapters/restServer/express.restServer";
+import { restServer } from "./adapters/restServer/interfaces/restServer.interface";
+import { serverConfig } from "./adapters/restServer/interfaces/serverConfig.type";
+
 
 const svrConfig: serverConfig = { port: 5005 };
 
